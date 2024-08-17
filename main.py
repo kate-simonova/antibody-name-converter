96c5e5595f097e3a68cccfd95373db3f0600d309
kate-simonova <simonova.fake@gmail.com>
2024-08-17 12:36:21 +0200

    css separated

--- main.py
+++ main.py
@@ -8,22 +8,12 @@ from dash.exceptions import PreventUpdate
 
 encode_disease_infixes = {value:key for key, value in disease_infixes.items()}
 
-# Define the GitHub link and icon
-github_link = "https://github.com/kate-simonova/antibody-name-converter"
+github_icon = html.A(href="https://github.com/kate-simonova", children=[
+    html.Img(className="github-icon", src="https://img.icons8.com/material-rounded/48/000000/github.png")
+], className="github-icon")
 
-github_icon = html.A(
-    href=github_link,
-    children=[
-        html.Img(src="https://img.icons8.com/material-rounded/48/000000/github.png", style={'width': '40px', 'height': '40px'})
-    ],
-    target="_blank",
-    style={'margin-right': '10px'}
-)
+footer = html.H3("Created by Ekaterina Simonova © 2024")
 
-h6_element = html.H6(
-    "Created by Ekaterina Simonova © 2024",
-    style={'textAlign': 'center', 'color': '#333333', 'font-size': '15px', 'font-family': 'cursive', 'margin': '0'}
-)
 
 # Define style for tabs
 tab_style = {
@@ -47,14 +37,17 @@ tab_selected_style = {
     'fontWeight': 'bold',
 }
 
-style_page = {
-    'backdrop-filter': 'blur(10px)',
-    'background-color': 'rgba(255, 255, 255, 0.5)',
-    'padding': '10px',
-    'margin': '20px auto',
-    'border-radius': '10px',
-    'width': '60%'
-}
+
+import pandas as pd
+from dash import Dash, html, dcc, dash_table, Input, Output, State, ctx
+from data import decode_molecule, disease_infixes, mab_new_names
+from dash.exceptions import PreventUpdate
+
+encode_disease_infixes = {value:key for key, value in disease_infixes.items()}
+
+# Define the GitHub link and icon
+github_link = "https://github.com/kate-simonova/antibody-name-converter"
+
 
 # Define the Dash app
 app = Dash(__name__)
@@ -72,46 +65,23 @@ app.layout = html.Div(
                     # Content of the Decoder tab here
                     html.Div(
                         children=[
-                            html.H1("Welcome to the Monoclonal Antibody Name Decoder", style={'textAlign': 'center', 'margin-bottom': '20px'}),
-                            dcc.Input(id='input-antibody', type='text', placeholder='Enter antibody name here', maxLength=50, style={'margin': '10px', 'padding': '10px', 'width': '400px'}),
-                            html.Button('Decode', id='decode-button', n_clicks=0, style={'margin': '10px', 'padding': '10px'}),
-                            html.Div(id='syllabified-name', style={'margin': '10px', 'padding': '10px', 'font-weight': 'bold', 'fontSize': '18px'}),
+                            html.H1("Welcome to the Monoclonal Antibody Name Decoder"),
+                            dcc.Input(id='input-antibody', type='text', placeholder='Enter antibody name here', maxLength=50),
+                            html.Button('Decode', id='decode-button', n_clicks=0),
+                            html.Div(id='syllabified-name'),
                             dash_table.DataTable(
                                 id='output-table',
-                                columns=[
-                                    {"name": "Part of Word", "id": "Part of Word"},
-                                    {"name": "Meaning", "id": "Meaning"}
-                                ],
                                 data=[],
-                                style_table={'margin': '10px', 'width': '33.33%'},
-                                style_header={'display': 'none'},
-                                style_cell={'textAlign': 'left', 'padding': '10px'},
-                                style_data_conditional=[
-                                    {
-                                        'if': {'row_index': 0},
-                                        'backgroundColor': '#83cec7',
-                                        'fontWeight': 'bold'
-                                    },
-                                    {
-                                        'if': {'column_id': 'Part of Word'},
-                                        'textAlign': 'center'
-                                    },
-                                    {
-                                        'if': {'column_id': 'Meaning'},
-                                        'textAlign': 'left'
-                                    }
-                                ],
                             ),
                         ],
-                        style= style_page,
+                        className='style-page',
                     ),
-                    # Footer with GitHub link and creator information
                 html.Div(
                     children=[
                         github_icon,
-                        h6_element
+                        footer
                     ],
-                    style={'display': 'flex', 'alignItems': 'center', 'justifyContent': 'center', 'margin-top': '20px'}
+                    className="footer-style"
                 )],
                 style=tab_style,
                 selected_style=tab_selected_style
@@ -123,48 +93,39 @@ app.layout = html.Div(
                 children=[
                     html.Div(
                         children=[
-                            html.H1("Welcome to the Monoclonal Antibody Name Generator", style={'textAlign': 'center'}),
-                            dcc.Input(id='prefix-input', type='text', placeholder='Input your prefix', maxLength=50, style={'margin': '15px 0 15px 25px', 'padding': '10px', 'width': '380px'}),
+                            html.H1("Welcome to the Monoclonal Antibody Name Generator"),
+                            dcc.Input(id='prefix-input', type='text', placeholder='Input your prefix', maxLength=50),
                             dcc.Dropdown(
                                 id='infix-dropdown',
                                 options=list(encode_disease_infixes.keys()),
                                 placeholder='Select Infix for target class',
-                                style={'margin': '10px', 'padding': '0px', 'width': '500px'}
+                                className='generator-dropdown',
                             ),
                             dcc.Dropdown(
                                 id='suffix-dropdown',
                                 options=list(mab_new_names.keys()),
                                 placeholder='Select suffix based on the immunoglobulin variable domain',
-                                style={'margin': '10px', 'padding': '0px', 'width': '500px'}
+                                className='generator-dropdown'
                             ),
-                            html.Button('Generate', id='generate-button', n_clicks=0, style={'margin': '15px 0 15px 25px', 'padding': '10px'}),
+                            html.Button('Generate', id='generate-button', n_clicks=0),
                             html.Br(),
-                            html.Div(id='antibody-name', style={'margin': '10px', 'padding': '10px', 'fontSize': '18px', "font-weight": "bold", 'display': 'inline-block'}),
+                            html.Div(id='antibody-name', className='gen-out'),
                         ],
-                        style= style_page,
+                        className='style-page',
                     ),
-                # Footer with GitHub link and creator information
                 html.Div(
                     children=[
                         github_icon,
-                        h6_element
+                        footer
                     ],
-                    style={'display': 'flex', 'alignItems': 'center', 'justifyContent': 'center', 'margin-top': '20px'}
+                    className="footer-style"
                 )],
                 style=tab_style,
                 selected_style=tab_selected_style
             ),
         ]),
     ),
-    style={
-        'backgroundImage': "url('/assets/diginex.jpg')",
-        'backgroundSize': 'cover',
-        'padding': '20px',
-        'border-radius': '0px',
-        'border': 'none',
-        'width': '100%',
-        'minHeight': '100vh'
-    }
+    className="layout-style",
 )
 
 # Callback function
